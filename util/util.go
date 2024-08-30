4526d2886d5566cb004598c4f0647cfdd2e43080
ryqdev <ykshang4119@gmail.com>
2024-08-30 13:16:42 +0800

    feat: add Ternary

--- util/util.go
+++ util/util.go
@@ -3,10 +3,22 @@ package util
 import "github.com/ryqdev/golang_utils/log"
 
 // IfThenElse is the conditional operator in Golang
+// Deprecated: use Ternary instead
 // Example:
 // x, y := 1, 2
 // max := IfThenElse(x > y, x, y).(int)
 func IfThenElse[T any](condition bool, trueValue, falseValue T) T {
+	if condition {
+		return trueValue
+	}
+	return falseValue
+}
+
+// Ternary is the conditional operator in Golang, the updated version of IfThenElse
+// Example:
+// x, y := 1, 2
+// max, ok := Ternary(x > y, x, y).(int)
+func Ternary[T any](condition bool, trueValue, falseValue T) T {
 	if condition {
 		return trueValue
 	}
